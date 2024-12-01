4b583f3e7bb3c87ea8fc30743f5d53fcd208c060
RCHD88 <richardraferguy@gmail.com>
2024-12-02 00:45:52 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  app.enableCors({
+    origin: 'http://localhost:5173',  // Ganti dengan URL frontend Anda
+    methods: 'GET, POST, PUT, DELETE', // Tambahkan metode yang diizinkan sesuai kebutuhan
+    allowedHeaders: 'Content-Type, Authorization',  // Header yang diizinkan
+  });
+
   await app.listen(3000);
 }
 bootstrap();
