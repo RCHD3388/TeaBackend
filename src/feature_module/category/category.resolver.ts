e1b58ecff34a0c843eb928c9cbe8d086e733af08
RCHD3388 <richardraferguy@gmail.com>
2024-12-04 16:56:02 +0700

    add auth

--- src/feature_module/category/category.resolver.ts
+++ src/feature_module/category/category.resolver.ts
@@ -2,22 +2,33 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { CategoryService } from './category.service';
 import { CategoryData } from './schema/category.schema';
 import { CreateCategoryInput, UpdateCategoryInput } from './types/category.types';
+import { UseGuards } from '@nestjs/common';
+import { AppAuthGuard } from '../user/auth_related/auth.guard';
+import { RolesGuard } from 'src/common/guard/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Resolver(() => CategoryData)
+@UseGuards(AppAuthGuard)
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) { }
 
+  @Mutation(() => CategoryData)
+  @UseGuards(RolesGuard)
+  @Roles("owner")
+  async createCategory(@Args('createCategoryInput') createCategoryInput: CreateCategoryInput): Promise<CategoryData> {
+    return this.categoryService.create(createCategoryInput);
+  }
+
   @Query(() => [CategoryData], { name: 'getCategories' })
+  @UseGuards(RolesGuard)
+  @Roles("owner")
   async getCategories(): Promise<CategoryData[]> {
     return this.categoryService.findAll();
   }
 
-  @Mutation(() => CategoryData)
-  async createCategory( @Args('createCategoryInput') createCategoryInput: CreateCategoryInput): Promise<CategoryData> {
-    return this.categoryService.create(createCategoryInput);
-  }
-
   @Mutation(() => CategoryData, { name: 'updateCategory' })
+  @UseGuards(RolesGuard)
+  @Roles("owner")
   async updateCategory(
     @Args('id') id: string,
     @Args('updateCategoryInput') updateCategoryInput: UpdateCategoryInput,
@@ -26,6 +37,8 @@ export class CategoryResolver {
   }
 
   @Mutation(() => CategoryData)
+  @UseGuards(RolesGuard)
+  @Roles("owner")
   async deleteCategory(@Args('id') id: string): Promise<CategoryData> {
     return this.categoryService.delete(id);
   }
